cc6a63fa568df9006c75dcbac48b35d622a06fa5
mandrak <mandrak@marcin_laptop>
2010-06-11 15:28:07 +0000

    Conky shows weather for later

--- scripts/pogoda.pl
+++ scripts/pogoda.pl
@@ -5,33 +5,48 @@ use HTML::TreeBuilder;
 use HTML::Element;
 use LWP::Simple;
 
-our $wiatr = "";
-our $temp = "";
-our $deszcz = "";
-our $cisnienie = "";
+our %wyniki;
+our $ilosc_spacji = 7; # ilosc spacji jaka dzieli dwa wyniki w conky
 
-system "rm pogoda.html";
-getstore("http://pogoda.interia.pl/miasta?id=11827", "pogoda.html");
+my $html = get("http://pogoda.interia.pl/miasta?id=11827");
 my $tree = HTML::TreeBuilder->new;
-$tree->parse_file("pogoda.html");
+$tree->parse($html);
+$tree->eof;
 my @elements = $tree->find("table");
 @elements = $elements[8]->find("td");
-if ($elements[5]->as_HTML =~ /Wiatr:\s+(\d+\s+km)/)
+if ($elements[5]->as_HTML =~ /Wiatr:\s+(\d+)/)
 {
-	$wiatr = $1;
+	$wyniki->{"teraz"}->{"wiatr"} = $1;
+}
+if ($elements[8]->as_HTML =~ /Wiatr:\s+(\d+)/)
+{
+	$wyniki->{"potem"}->{"wiatr"} = $1;
 }
 if ($elements[10]->as_HTML =~ m{<b>(\d+)</b>.*?>(\d+)<})
 {
-	$temp = $2;
+	$wyniki->{"teraz"}->{"temperatura"} = $2;
 }
-if ($elements[15]->as_HTML =~ m{<b>(.+)</b>})
+if ($elements[12]->as_HTML =~ m{<b>(\d+)</b>.*?>(\d+)<})
 {
-	$deszcz = $1;
+	$wyniki->{"potem"}->{"temperatura"} = $2;
+}
+if ($elements[15]->as_HTML =~ m{<b>(.+) mm</b>})
+{
+	$wyniki->{"teraz"}->{"deszcz"} = $1;
+}
+if ($elements[17]->as_HTML =~ m{<b>(.+) mm</b>})
+{
+	$wyniki->{"potem"}->{"deszcz"} = $1;
+}
+if ($elements[14]->as_HTML =~ m{<b>(\d+)</b>})
+{
+	$wyniki->{"teraz"}->{"cisnienie"} = $1;
 }
 if ($elements[16]->as_HTML =~ m{<b>(\d+)</b>})
 {
-	$cisnienie = $1;
+	$wyniki->{"potem"}->{"cisnienie"} = $1;
 }
+
 #obrazek obrazujacy aktualny stan pogody
 if ($elements[6]->as_HTML =~ m/src="(.+?)"/)
 {
@@ -41,19 +56,19 @@ foreach(@ARGV)
 {
 	if ($_ eq "-w")
 	{
-		print $wiatr;
+		print $wyniki->{"teraz"}->{"wiatr"} . " "x ($ilosc_spacji - length($wyniki->{"teraz"}->{"wiatr"})) . $wyniki->{"potem"}->{"wiatr"};
 	}
 	if ($_ eq "-r")
 	{
-		print $deszcz;
+		print $wyniki->{"teraz"}->{"deszcz"} . " " x ($ilosc_spacji - length($wyniki->{"teraz"}->{"deszcz"})). $wyniki->{"potem"}->{"deszcz"};
 	}
 	if ($_ eq "-p")
 	{
-		print $cisnienie . "hPa";
+		print $wyniki->{"teraz"}->{"cisnienie"} . " " x ($ilosc_spacji - length($wyniki->{"teraz"}->{"cisnienie"})). $wyniki->{"potem"}->{"cisnienie"};
 	}
 	if ($_ eq "-t")
 	{
-		print $temp . "C";
+		print $wyniki->{"teraz"}->{"temperatura"} . " " x ($ilosc_spacji - length($wyniki->{"teraz"}->{"temperatura"})). $wyniki->{"potem"}->{"temperatura"};
 	}
 	print "\n";
 }
