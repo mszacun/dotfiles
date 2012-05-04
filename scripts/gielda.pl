54ef6f1211caba5efddb9f29e11f97a0702d19c7
szacun <szacun@mail.ru>
2012-05-04 20:02:57 +0200

    Save stocks informations to file
    They can be used when there is no Internet connectivity

--- scripts/gielda.pl
+++ scripts/gielda.pl
@@ -3,49 +3,66 @@
 use warnings;
 use strict;
 
-use LWP::Simple;
+use LWP::UserAgent;
 use HTML::Entities;
+use YAML; 
 
-our %wig20;
-our %mwig40;
-our %swig80;
-our %wig;
+our $wyniki;
+my $infos;
 
 my @time = localtime;
 exit if ($time[6] > 5 || $time[6] == 0);
 
-my $content = get("http://mojeinwestycje.interia.pl/gie/notgpw/notc/c_akcje");
-$content =~ s/&nbsp;/ /g;
+if (-e "/tmp/economy.yaml")
+{
+	$infos = YAML::LoadFile("/tmp/economy.yaml")
+}
 
-print "\n";
-if ($content =~ m{.*?<b>WIG20</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
+my $ua = LWP::UserAgent->new;
+my $response = $ua->get("http://mojeinwestycje.interia.pl/gie/notgpw/notc/c_akcje");
+if ($response->is_success)
 {
-	$wig20{"zmiana"} = $4 . "%";
-	$wig20{"obroty"} = $6;
-	$wig20{"wartosc"} = $3;
+	my $content = $response->decoded_content;
+	$content =~ s/&nbsp;/ /g;
+
+	print "\n";
+	if ($content =~ m{.*?<b>WIG20</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
+	{
+		$wyniki->{WIG20}->{"zmiana"} = $4 . "%";
+		$wyniki->{WIG20}->{"obroty"} = $6;
+		$wyniki->{WIG20}->{"wartosc"} = $3;
+	}
+	if ($content =~ m{.*?<b>mWIG40</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
+	{
+		$wyniki->{mWIG40}->{"zmiana"} = $4 . "%";
+		$wyniki->{mWIG40}->{"obroty"} = $6;
+		$wyniki->{mWIG40}->{"wartosc"} = $3;
+	}
+	if ($content =~ m{.*?<b>WIG</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
+	{
+		$wyniki->{WIG}->{"zmiana"} = $4 . "%";
+		$wyniki->{WIG}->{"obroty"} = $6;
+		$wyniki->{WIG}->{"wartosc"} = $3;
+	}
+	if ($content =~ m{.*?<b>sWIG80</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
+	{
+		$wyniki->{sWIG80}->{"zmiana"} = $4 . "%";
+		$wyniki->{sWIG80}->{"obroty"} = $6;
+		$wyniki->{sWIG80}->{"wartosc"} = $3;
+	}
 }
-if ($content =~ m{.*?<b>mWIG40</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
+else
 {
-	$mwig40{"zmiana"} = $4 . "%";
-	$mwig40{"obroty"} = $6;
-	$mwig40{"wartosc"} = $3;
-}
-if ($content =~ m{.*?<b>WIG</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
-{
-	$wig{"zmiana"} = $4 . "%";
-	$wig{"obroty"} = $6;
-	$wig{"wartosc"} = $3;
-}
-if ($content =~ m{.*?<b>sWIG80</b></a>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?<b>(.*?)</b>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>}s)
-{
-	$swig80{"zmiana"} = $4 . "%";
-	$swig80{"obroty"} = $6;
-	$swig80{"wartosc"} = $3;
+	$wyniki = $infos->{Gielda};
 }
 
 print "   +-- \${color #4477AA}Stocks: \${color yellow}\n";
-print "   |    +-- \${color #888888} WIG:  \${color #CCCCCC} $wig{wartosc} $wig{zmiana} $wig{obroty} \${color yellow}\n";
-print "   |    +-- \${color #888888} WIG20: \${color #CCCCCC} $wig20{wartosc} $wig20{zmiana} $wig20{obroty} \${color yellow}\n";
-print "   |    +-- \${color #888888} sWIG40:\${color #CCCCCC} $mwig40{wartosc} $mwig40{zmiana} $mwig40{obroty} \${color yellow}\n";
-print "   |    +-- \${color #888888} mWIG80:\${color #CCCCCC} $swig80{wartosc} $swig80{zmiana} $swig80{obroty} \${color yellow}\n";
+print "   |   +-- \${color #888888} WIG:  \${color #CCCCCC} $wyniki->{WIG}->{wartosc} $wyniki->{WIG}->{zmiana} $wyniki->{WIG}->{obroty} \${color yellow}\n";
+print "   |   +-- \${color #888888} WIG20: \${color #CCCCCC} $wyniki->{WIG20}->{wartosc} $wyniki->{WIG20}->{zmiana} $wyniki->{WIG20}->{obroty} \${color yellow}\n";
+print "   |   +-- \${color #888888} sWIG40:\${color #CCCCCC} $wyniki->{mWIG40}->{wartosc} $wyniki->{mWIG40}->{zmiana} $wyniki->{mWIG40}->{obroty} \${color yellow}\n";
+print "   |   +-- \${color #888888} mWIG80:\${color #CCCCCC} $wyniki->{sWIG80}->{wartosc} $wyniki->{sWIG80}->{zmiana} $wyniki->{sWIG80}->{obroty} \${color yellow}\n";
+print "\n";
 
+my $hash = {Gielda => $wyniki};
+
+YAML::DumpFile("/tmp/economy.yaml", $hash);
