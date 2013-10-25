2f99cf70422015036bf73393e3e4cca7e7e511c6
szacun <szacun@mail.ru>
2013-10-25 17:19:44 +0200

    Modify start.sh to be able to change CPU frequency

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 hdparm -B 255 /dev/sda
 hdparm -S0 /dev/sda
 smartctl -d ata -a /dev/sda | grep Load_Cycle_Count 
-# modprobe acpi-cpufreq
+modprobe acpi-cpufreq
 # cpufreq-set -g ondemand
 rmmod pcspkr
+cpupower frequency-set -g powersave
+cpupower frequency-set -u 1500MHz
