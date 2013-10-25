99f3b4139a2c469ef217a3a464ab9096b99581f4
szacun <szacun@mail.ru>
2013-10-25 17:17:59 +0200

    Install fluxbox on new computer, adjust configurations files to new resolution

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mount -t ecryptfs /home/mandrak /home/mandrak
+# mount -t ecryptfs /home/mandrak /home/mandrak
 hdparm -B 255 /dev/sda
 hdparm -S0 /dev/sda
 smartctl -d ata -a /dev/sda | grep Load_Cycle_Count 
-modprobe acpi-cpufreq
-cpufreq-set -g ondemand
+# modprobe acpi-cpufreq
+# cpufreq-set -g ondemand
 rmmod pcspkr
