1ee534a70b80c403fbe1c0f4446017194a120802
Marcin Szachun <mszachun@gmail.com>
2015-11-06 18:53:23 +0100

    Install oh-my-zsh and copy tmux configuration file in instalation script

--- install.sh
+++ install.sh
@@ -15,3 +15,11 @@ ln ~/dotfiles/conkyrc ~/.conkyrc
 
 # copy scripts used in conky
 ln -s ~/dotfiles/scripts ~/.scripts
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+mkdir ~/.oh-my-zsh/custom/themes
+ln -s ~/dotfiles/zsh/mytheme.zsh-theme ~/.oh-my-zsh/custom/themes
+
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
