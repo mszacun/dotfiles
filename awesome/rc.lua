e8f78fda4a7c44dff8b9af28573a04e6cb611f17
Marcin Szachun <mszachun@gmail.com>
2016-01-21 20:37:26 +0100

    Add powerline to awesome bar

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -11,6 +11,9 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 
+package.path = package.path .. ';/usr/lib/python2.7/site-packages/powerline/bindings/awesome/?.lua'
+local powerline = require('powerline')
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -197,6 +200,7 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(powerline_widget)
     right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
