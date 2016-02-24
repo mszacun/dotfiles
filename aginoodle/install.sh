8459e64bf136a50d97c01370b6b7ed63ff6aa4e5
Marcin Szachun <marcin.szachun@nokia.com>
2016-02-24 17:44:19 +0100

    Add aginoodle instalation script

---
+++ aginoodle/install.sh
@@ -0,0 +1,19 @@
+ln ~/dotfiles/aginoodle/secret.py $WORKSPACE/src/backlog/settings/secret.py
+ln -s ~/dotfiles/aginoodle/exclude $WORKSPACE/.git/info/exclude
+ln -s ~/dotfiles/aginoodle/snippets $WORKSPACE/snippets
+
+sudo pacman -S mariadb gcc
+
+sudo pip2 install mycli django-extensions
+
+# install arc
+mkdir ~/bin
+cd ~/bin
+git clone https://github.com/phacility/libphutil.git
+git clone https://github.com/phacility/arcanist.git
+echo ‘export PATH="$PATH:~/bin/arcanist/bin"’ >> ~/.bashrc
+cd ~/bin/libphutil
+git apply ~/dotfiles/aginoodle/arc_fix_patch.diff
+cd $WORKSPACE
+~/bin/arcanist/bin/arc install_certificate
+
