89864f583a99b491ce5ced44f614d52137d53c37
Marcin Szachun <mszachun@gmail.com>
2017-04-24 21:28:42 +0200

    Don't install arc when installing aginoodle

--- aginoodle/install.sh
+++ aginoodle/install.sh
@@ -18,14 +18,3 @@ php56 -t ~/teamcal -S 127.0.0.1:4000&
 sleep 2
 curl 'http://127.0.0.1:4000/installation.php' -H 'Host: 127.0.0.1:4000' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://127.0.0.1:4000/installation.php' -H 'Connection: keep-alive' --data 'txt_instAppRelDir=%2F&txt_instAppURL=http%3A%2F%2F127.0.0.1%3A4000%2F&txt_instDbServer=127.0.0.1&txt_instDbName=teamcal&txt_instDbUser=root&txt_instDbPassword=&txt_instDbPrefix=my_&opt_data=none&chkLicGpl=chkLicGpl&chkLicTcpro=chkLicTcpro&btn_install=Install'
 
-# install arc
-mkdir ~/bin
-cd ~/bin
-git clone https://github.com/phacility/libphutil.git
-git clone https://github.com/phacility/arcanist.git
-echo ‘export PATH="$PATH:~/bin/arcanist/bin"’ >> ~/.bashrc
-cd ~/bin/libphutil
-git apply ~/dotfiles/aginoodle/arc_fix_patch.diff
-cd $WORKSPACE
-~/bin/arcanist/bin/arc install_certificate
-
