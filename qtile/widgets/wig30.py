ca319d372478bdf7a8f940f48d244eb92f279520
Marcin Szachun <marcin.szachun@nokia.com>
2019-04-18 18:28:47 +0200

    Fix WIG30 widget

--- qtile/widgets/wig30.py
+++ qtile/widgets/wig30.py
@@ -2,8 +2,13 @@ from libqtile.widget.generic_poll_text import GenPollUrl
 from bs4 import BeautifulSoup
 
 
+def parse(source):
+    soup = BeautifulSoup(source, 'html.parser')
+    return soup.select('#boxProfilHeader .change .value')[0].text
+
+
 class Wig30Widget(GenPollUrl):
-    url = 'http://www.money.pl/gielda/indeksy_gpw/wig30/'
+    url = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=WIG30'
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'
     up_icon = u'\uf176'
     down_icon = u'\uf175'
@@ -13,8 +18,7 @@ class Wig30Widget(GenPollUrl):
     green = 'A1B56C'
 
     def parse(self, source):
-        soup = BeautifulSoup(source, 'html.parser')
-        index_change = soup.select('.tabela td')[3].text
+        index_change = parse(source)
         is_down = index_change[0] == '-'
         content = self.down_icon if is_down else self.up_icon
         content += index_change
