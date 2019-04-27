2111ac4c5657d876be523f999e11ecba81525436
Marcin Szachun <marcin.szachun@nokia.com>
2019-04-27 22:29:22 +0200

    Configure storing local and global todo lists using todo.txt

---
+++ zsh/todo.zsh
@@ -0,0 +1,13 @@
+GLOBAL_TODO_DIR="$HOME/.todo/"
+
+alias ta="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
+alias tl="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper ls"
+alias tdo="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper do"
+alias tp="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper p"
+
+
+alias lt="TODO_DIR=./todo/ todo.sh -t"
+alias lta="TODO_DIR=./todo/ todo.sh -t add"
+alias ltl="TODO_DIR=./todo/ todo.sh ls"
+alias ltdo="TODO_DIR=./todo/ todo.sh do"
+alias ltp="TODO_DIR=./todo/ todo.sh p"
