e2f7e4700c71685ecceab8d702e3357c044731dd
Marcin Szachun <marcin.szachun@nokia.com>
2019-05-10 01:05:47 +0200

    Add aliases for adding tasks with priority

--- zsh/todo.zsh
+++ zsh/todo.zsh
@@ -1,6 +1,12 @@
 GLOBAL_TODO_DIR="$HOME/.todo/"
 
+alias t="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper"
 alias ta="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
+alias taa="TODOTXT_PRIORITY_ON_ADD=A TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
+alias tab="TODOTXT_PRIORITY_ON_ADD=B TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
+alias tac="TODOTXT_PRIORITY_ON_ADD=C TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
+alias tad="TODOTXT_PRIORITY_ON_ADD=D TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
+alias tae="TODOTXT_PRIORITY_ON_ADD=E TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper -t add"
 alias tl="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper ls"
 alias tdo="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper do"
 alias tp="TODO_DIR=$GLOBAL_TODO_DIR todo-git-wrapper p"
