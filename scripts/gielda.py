b0df95c4b2b4cdacb63de4faca81d19971622aef
Marcin Szachun <marcin.szachun@nokia.com>
2019-05-22 09:46:41 +0200

    Show price direction in stocks script

--- scripts/gielda.py
+++ scripts/gielda.py
@@ -1,9 +1,18 @@
 import sys
+import os
+import json
 
 import requests
 from bs4 import BeautifulSoup
 
 from texttable import Texttable
+
+UP_ARROW = '\033[92m\uf176\033[0m'
+DOWN_ARROW = '\033[91m\uf175\033[0m'
+
+
+PREVIOUS_RUN_FILE_PATH = os.path.join(os.path.expanduser('~/.stocks.json'))
+
 
 def get_company_from_row(company_row):
     row = company_row.select('td')
@@ -23,6 +32,13 @@ def get_company_from_row(company_row):
 def parse_percent(percent_str):
     return float(percent_str[:-1].replace(',', '.'))
 
+def get_direction(company, previous_run):
+    name = company['name']
+    price = float(company['price'].replace(',', '.'))
+    previous_price = float(previous_run[name].replace(',', '.'))
+
+    return UP_ARROW if price > previous_price else DOWN_ARROW
+
 requested_companies = sys.argv[1:]
 
 html = requests.get('https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=WIG').content
@@ -36,11 +52,18 @@ sorted_by_price_change = sorted(filtered_companys, key=lambda company: parse_per
 
 table = Texttable()
 table.set_deco(Texttable.HEADER)
-table.set_cols_dtype(['t', 'f', 'f'])
-table.set_cols_align(["l", "r", "r"])
-table.header(['Name', 'Current Price', 'Percent Change'])
+table.set_cols_dtype(['t', 'f', 'f', 't'])
+table.set_cols_align(["l", "r", "r", 'c'])
+table.header(['Name', 'Current Price', 'Percent Change', 'Direction'])
+
+with open(PREVIOUS_RUN_FILE_PATH, 'r') as f:
+    previous_run = json.load(f)
 
 for company in sorted_by_price_change:
-    table.add_row([company['name'], company['price'], company['percent_change']])
+    table.add_row([company['name'], company['price'], company['percent_change'], get_direction(company, previous_run)])
+    previous_run[company['name']] = company['price']
+
+with open(PREVIOUS_RUN_FILE_PATH, 'w') as f:
+    json.dump(previous_run, f, indent=4, sort_keys=True)
 
 print(table.draw())
