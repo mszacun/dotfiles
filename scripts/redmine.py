b7baee318961fcfc3c0f68bb5dd5ea03c7bb4ef0
Marcin Szachun <marcin.szachun@identt.pl>
2020-03-13 13:36:21 +0100

    Update redmine script, allow changing status to "In review"

--- scripts/redmine.py
+++ scripts/redmine.py
@@ -9,6 +9,12 @@ from redminelib import Redmine
 import iterfzf
 
 
+class StatusPrefetchingRedmine(Redmine):
+    def __init__(self, *args, **kwargs):
+        super(StatusPrefetchingRedmine, self).__init__(*args, **kwargs)
+
+        self.statuses = {status.name: status for status in self.issue_status.all()}
+
 
 PASS_ADDITIONAL_ENTRY_REGEXP = re.compile('(\w+): (.*)')
 
@@ -21,6 +27,12 @@ def _get_credentials_from_password_store(pass_entry):
             result[match.group(1)] = match.group(2)
 
     return result
+
+
+def extract_issue_from_branch_name():
+    branch_name = subprocess.check_output('git branch --show-current'.split()).decode().strip()
+    without_prefix = branch_name[branch_name.index('/') + 1:]
+    return without_prefix[:without_prefix.index('-')]
 
 
 def select_using_fzf(options, key=None):
@@ -40,7 +52,7 @@ def select_issue(issues):
 
 credentials = _get_credentials_from_password_store('identt/redmine')
 
-redmine = Redmine(credentials['url'], username=credentials['login'], password=credentials['password'])
+redmine = StatusPrefetchingRedmine(credentials['url'], username=credentials['login'], password=credentials['password'])
 user = redmine.user.get(resource_id=credentials['user_id'])
 
 
@@ -57,16 +69,33 @@ def log_time(args):
     redmine.time_entry.create(issue_id=selected_issue.id, hours=8)
 
 
+def show_issue(args):
+    issue_from_current_branch = extract_issue_from_branch_name()
+    url = '{}/issues/{}'.format(credentials['url'], issue_from_current_branch)
+    subprocess.run(['firefox', url])
+
+
+def review(args):
+    issue_from_current_branch = extract_issue_from_branch_name()
+    issue = redmine.issue.get(resource_id=issue_from_current_branch)
+    issue.status_id = redmine.statuses['In review']
+    issue.save()
+
+
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
 parser_work = subparsers.add_parser('work')
 parser_work.set_defaults(func=start_work)
 
-parser_work = subparsers.add_parser('log_time')
-parser_work.set_defaults(func=log_time)
+parser_log_time = subparsers.add_parser('log-time')
+parser_log_time.set_defaults(func=log_time)
+
+parser_show_issue = subparsers.add_parser('show-issue')
+parser_show_issue.set_defaults(func=show_issue)
+
+parser_review = subparsers.add_parser('review')
+parser_review.set_defaults(func=review)
 
 args = parser.parse_args()
 args.func(args)
-
-
