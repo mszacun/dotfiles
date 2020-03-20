b05d148c7b2507e30c2f64011dda3e559ae07415
Marcin Szachun <marcin.szachun@identt.pl>
2020-03-20 16:50:27 +0100

    Remove old powerline configuration

--- powerline/install.sh
+++
@@ -1,5 +0,0 @@
-sudo cp segments/* /usr/lib/python2.7/site-packages/powerline/segments/common
-sudo cp powerline.lua /usr/lib/python2.7/site-packages/powerline/bindings/awesome/powerline.lua
-
-mkdir ~/.config/powerline
-cp -r colors.json colorschemes config.json themes ~/.config/powerline
