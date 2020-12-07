c53c7bd15e4f7a145614cc09e70b3c53e12391f9
Marcin Szachun <marcin.szachun@identt.pl>
2020-12-07 14:31:52 +0100

    Make timewarrior widget display notification when time is not tracked during work

--- qtile/widgets/timew.py
+++ qtile/widgets/timew.py
@@ -1,4 +1,5 @@
 import subprocess
+from datetime import datetime, timedelta
 
 from libqtile.widget import base
 from libqtile.log_utils import logger
@@ -6,6 +7,12 @@ from libqtile.log_utils import logger
 
 class Timew(base.ThreadedPollText):
     update_interval = 1.0
+    notification_interval = timedelta(minutes=10)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.last_notification = datetime.now()
 
     def poll(self):
         try:
@@ -17,8 +24,24 @@ class Timew(base.ThreadedPollText):
                 return f'{task_name} - {total_time}'
         except subprocess.CalledProcessError as e:
             self.running = False
+            self._display_no_time_tracking_notification()
             return ''
 
     def button_press(self, x, y, button):
         if self.running:
             subprocess.run(['timew', 'stop'])
+
+    def _display_no_time_tracking_notification(self):
+        current_time = datetime.now()
+        if self._is_working_day(current_time) and self._is_working_hours(current_time) and self._enough_time_since_last_notification(current_time):
+            subprocess.run(['notify-send', '-u', 'CRITICAL', 'Timew', 'No time tracking in progress'])
+            self.last_notification = datetime.now()
+
+    def _is_working_day(self, t):
+        return t.weekday() not in [5, 6]
+
+    def _is_working_hours(self, t):
+        return 8 < t.hour < 18
+
+    def _enough_time_since_last_notification(self, t):
+        return (t - self.last_notification) >= self.notification_interval
